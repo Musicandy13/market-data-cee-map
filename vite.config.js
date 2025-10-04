3ca23c4d66749b5b9250ec111d6cf892aa35d1b8
Musicandy13 <132613852+Musicandy13@users.noreply.github.com>
2025-10-04 19:31:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.cjs',
+    // erzwinge absoluten Pfad zu deiner .cjs-Datei
+    postcss: path.resolve(__dirname, 'postcss.config.cjs'),
   },
   build: {
     outDir: 'dist',
     sourcemap: false,
-  },
-  server: {
-    port: 5173,
-    open: true,
   },
 })
