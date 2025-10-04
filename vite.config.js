71b8a41f610bfc34e07c287e8c58200238e959be
Musicandy13 <132613852+Musicandy13@users.noreply.github.com>
2025-10-04 19:25:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    // Erzwinge, dass Vite explizit deine CommonJS-PostCSS-Konfiguration nutzt:
+    postcss: './postcss.config.cjs',
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 })
